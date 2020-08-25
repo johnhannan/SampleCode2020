aed2581c4666484dfdce6fd91ad0ad8949767a69
John Hannan <hannan@cse.psu.edu>
2020-08-25 13:22:07 -0400

    Our First Commit

---
+++ SwiftUIExamples/SwiftUIExamples/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  SwiftUIExamples
+//
+//  Created by jjh9 on 8/25/20.
+//  Copyright © 2020 jjh9. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Image("Mascot0")
+            VStack {
+                Text("We Are!")
+                Text("Penn State")
+            }
+            VStack {
+                Group {
+                Text("Thank You")
+                Text("You're Welcome")
+                }
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
